4e9bdb749e55f1a84483cc626ae7deb5f2610b16
alexandre.rangeraluffy74@gmail.com <alexandre.rangeraluffy74@gmail.com>
2021-04-29 16:33:18 +0200

    Routes log_in missing a S to sessions

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -1,4 +1,5 @@
 class AccountController < ApplicationController
+
   def edit
     @user = User.first
   end
--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,6 @@ Rails.application.routes.draw do
 
   #Login and sign up routes
   get 'sign_up', to: 'users#new', as: 'signup'
-  get 'login_in', to: 'session#new', as: 'login'
+  get 'log_in', to: 'sessions#new', as: 'login'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
