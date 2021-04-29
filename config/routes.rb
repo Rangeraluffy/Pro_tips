6f35c90764b050f40a1860f7c268182e1013f7d1
alexandre.rangeraluffy74@gmail.com <alexandre.rangeraluffy74@gmail.com>
2021-04-29 10:36:30 +0200

    Login and sign up routes

--- config/routes.rb
+++ config/routes.rb
@@ -17,5 +17,9 @@ Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
 
   resources :users
+
+  #Login and sign up routes
+  get 'sign_up', to: 'users#new', as: 'signup'
+  get 'login_in', to: 'session#new', as: 'login'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
