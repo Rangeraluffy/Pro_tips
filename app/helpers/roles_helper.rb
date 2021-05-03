58440b15e61627e742a8574b78301ee4fba87907
alexandre.rangeraluffy74@gmail.com <alexandre.rangeraluffy74@gmail.com>
2021-05-03 15:01:05 +0200

    Module RolesHelper

--- app/helpers/roles_helper.rb
+++ app/helpers/roles_helper.rb
@@ -1,3 +1,17 @@
 module RolesHelper
+    def can_edit_tip?(tip)
+      case current_user.role
+      when 'admin' then true
+      when 'registered' then tip.user == current_user
+      else false
+    end
+  end
 
+    def can_edit_user?(user)
+      case current_user.role
+      when 'admin' then true
+      when 'registered' then user == current_user
+      else false
+    end
+  end
 end
