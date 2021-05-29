53a7d4854f40747ecfba6635402ba217abb76e76
alexandre.rangeraluffy74@gmail.com <alexandre.rangeraluffy74@gmail.com>
2021-05-29 16:40:09 +0200

    Run production.sh

---
+++ run-production.sh
@@ -0,0 +1,2 @@
+export SECRET_KEY_BASE=05c1f3e2afa2e9e2dd28434c335b4e5989f67f89f2a3c72b9ff6fe3d7f2648231324c63f15bdf9f5cdfd8a21e4c4e06168f63400d70f741713a5dac7c316adc6
+rails server --environment production
