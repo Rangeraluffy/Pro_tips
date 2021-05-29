844b43a6ddcabe124c9da625f244313bcc55fbef
alexandre.rangeraluffy74@gmail.com <alexandre.rangeraluffy74@gmail.com>
2021-05-29 17:09:32 +0200

    Run-Production / Adding RAILS_SERVE_STATIC_FILES=true

--- run-production.sh
+++ run-production.sh
@@ -1,2 +1,3 @@
 export SECRET_KEY_BASE=05c1f3e2afa2e9e2dd28434c335b4e5989f67f89f2a3c72b9ff6fe3d7f2648231324c63f15bdf9f5cdfd8a21e4c4e06168f63400d70f741713a5dac7c316adc6
+export RAILS_SERVE_STATIC_FILES=true
 rails server --environment production
