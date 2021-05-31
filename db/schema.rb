7ed3a4e1b97a373385f219f1108f400c897ee272
alexandre.rangeraluffy74@gmail.com <alexandre.rangeraluffy74@gmail.com>
2021-05-31 22:39:50 +0200

    wip

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210529140807) do
+ActiveRecord::Schema.define(version: 20210531201655) do
 
   create_table "comments", force: :cascade do |t|
     t.string "body"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20210529140807) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "role"
   end
 
 end
