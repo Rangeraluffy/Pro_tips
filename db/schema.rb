c417a6fca512ca9215b90ef0fa636288502b5a90
alexandre.rangeraluffy74@gmail.com <alexandre.rangeraluffy74@gmail.com>
2021-06-02 12:02:46 +0200

    wip

--- db/schema.rb
+++ db/schema.rb
@@ -10,39 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210531201655) do
-
-  create_table "comments", force: :cascade do |t|
-    t.string "body"
-    t.integer "user_id"
-    t.integer "tip_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["tip_id"], name: "index_comments_on_tip_id"
-    t.index ["user_id"], name: "index_comments_on_user_id"
-  end
-
-  create_table "tips", force: :cascade do |t|
-    t.string "title"
-    t.string "body"
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_tips_on_user_id"
-  end
-
-  create_table "tips_users", id: false, force: :cascade do |t|
-    t.integer "tip_id", null: false
-    t.integer "user_id", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "email"
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "password_digest"
-    t.string "role"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
