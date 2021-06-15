eed3c55bff61e2f287364bf5bca684c25cd049de
alexandre.rangeraluffy74@gmail.com <alexandre.rangeraluffy74@gmail.com>
2021-06-15 15:28:13 +0200

    Organize models

--- app/models/tip.rb
+++ app/models/tip.rb
@@ -1,12 +1,11 @@
 class Tip < ApplicationRecord
-  belongs_to :user
-  has_many   :comments
-
-  has_and_belongs_to_many :users
-
   validates :title, presence: true,
                     length: { maximum: 100 }
   validates :body,  presence: true
+
+  belongs_to :user
+  has_many   :comments
+  has_and_belongs_to_many :users
 
   scope :most_recent_active, -> { order(updated_at: :desc).limit(4) }
   scope :title_contains, ->(term) { where('title LIKE ?', "%#{term}%") }
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ApplicationRecord
-  has_many :tips
-  has_many :comments
-  has_and_belongs_to_many :favourites, class_name: Tip.name
-
   validates :email, presence: true,
                     uniqueness: true
 
   after_initialize :default_role!
+
+  has_many :tips
+  has_many :comments
+  has_and_belongs_to_many :favourites, class_name: Tip.name
 
 #Add the has_secure_password in user class
     has_secure_password
