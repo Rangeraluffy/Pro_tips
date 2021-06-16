ade815c57af6537acb038aff7322ca9047cf2786
alexandre.rangeraluffy74@gmail.com <alexandre.rangeraluffy74@gmail.com>
2021-06-16 10:41:40 +0200

    Update models and organize

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,12 @@
 class User < ApplicationRecord
+  before_validation :downcase_email
+  after_initialize :default_role!
+
   validates :email, presence: true,
                     uniqueness: true
 
-  after_initialize :default_role!
-
-  has_many :tips
-  has_many :comments
+  has_many :tips, dependent: :destroy
+  has_many :comments, dependent: :destroy
   has_and_belongs_to_many :favourites, class_name: Tip.name
 
 #Add the has_secure_password in user class
@@ -18,6 +19,10 @@ class User < ApplicationRecord
     mount_uploader :avatar, AvatarUploader
 
     private
+
+    def downcase_email
+      self.email = email.downcase
+    end
 
     def default_role!
       self.role ||= 'registered'
